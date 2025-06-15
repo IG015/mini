db617a6c54613ee7e4e4d35295f610d9e41e417e
iago <iago.rodrigues325@gmail.com>
2025-06-15 03:17:04 +0100

    hello word

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"view","index.html"))
+})
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname,"view","sobre.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+  })
+
+  app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+  })
+  
+  app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+  })
+      
