cd802337179d2cf65a48bed3c91259ae361e71db
Davi Neves <davi.neves.santos2@gmail.com>
2025-06-15 04:07:44 +0100

    feature - implemented tailwindcss and concurrently

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"view","index.html"))
 })
